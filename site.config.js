357c4815db26fa862516154539f48f3243f84dad
Flavius <flavius.moldovan@zero-code.ro>
2024-12-31 12:03:13 +0200

    changed the description

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     name: "mrleritaite",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Machine Learning Engineer",
-    bio: "Building AI Agents. Interested in quantization and making llm runs faster. Building a startup.",
+    bio: "Building AI Agents. Interested in making llm runs faster. Building a startup.",
     email: "mrleritaite@gmail.com",
     linkedin: "",
     github: "MrLeritaite",
@@ -24,7 +24,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://mrleritaite-blog.vercel.app/s",
   since: "2025", // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
